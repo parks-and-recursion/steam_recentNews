f43970867dd5e19f36a41aa192f48bfe3d35782f
Grahams Laptop <doughertyg37@gmail.com>
2019-05-20 02:31:27 -0700

    fixed seed script, set sync option force to false

--- schema.sql
+++ schema.sql
@@ -1,7 +1,11 @@
 -- run from terminal using "mysql -u root < schema.sql" or "npm run initialize" to initialize the database
 DROP DATABASE IF EXISTS steam_updates;
+
 CREATE DATABASE steam_updates;
 
 USE steam_updates;
 
 -- seed script uses force: true and thus we don't need to create table here 
+CREATE TABLE updates (
+
+);
--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
 const faker = require('faker');
 const db = require('./database');
 
-db.Updates.sync({ force: true }) // use force to drop tables if exists
+db.Updates.sync({ force: false }) // force drops the tables and then won't seed successfully, set to false
 .then(() => {
 	console.log('tables dropped and synced');
 })
@@ -12,10 +12,10 @@ for (var i = 0; i < 100; i++) { //create 100 entries and fake content
 
 	updates.push({
 		id: faker.random.number(),
-		posted_by: faker.random.FunnyName.name().split(' ').join('_'),
-		post_date: faker.date(),
-		title: faker.Lorem.sentence(4, false, 4),
-		text: faker.Lorem.paragraph(3, false, 5),
+		posted_by: faker.internet.userName().split(' ').join('_'),
+		post_date: faker.date.past(),
+		title: faker.lorem.sentence(4, false, 4),
+		text: faker.lorem.paragraph(3, false, 5),
 		img: faker.image.abstract(), //not all stories need images
 		comment_count: faker.random.number()
 	})
